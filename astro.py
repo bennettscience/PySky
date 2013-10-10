4704f0fbd0bc264956fe308e2f813f9c98712e42
TSCBennett <b.bennett@techsmith.com>
2013-10-10 16:33:31 -0400

    Return ra/dec
    Changed the location return from alt/az to ra/dec becuase of errors in
    alt/az data.

--- astro.py
+++ astro.py
@@ -6,12 +6,12 @@ def const(planet_name):            # function name and parameters
     date_class = datetime.now()
     planet = planet_class()                     # sets planet variable 
     south_bend = ephem.Observer()               # Creates the Observer object 
-    south_bend.lat = '41.67'                    # latitude 
-    south_bend.lon = '-86.26'                   # west is negative
+    south_bend.lat = '41.40'                    # latitude 
+    south_bend.lon = '-86.15'                   
     south_bend.date = date_class                # sets date parameter
     planet.compute(south_bend)                  # calculates the location data
     print date_class
-    print planet.alt, planet.az
+    print planet.ra, planet.dec
     return ephem.constellation((planet.ra, planet.dec))
 
 print const(raw_input('Planet: '))
