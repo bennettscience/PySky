18de1e4026839a48498247841e462233c12ef603
TSCBennett <b.bennett@techsmith.com>
2013-10-10 15:36:25 -0400

    Added automatic date and time collection with `import datetime` module.
    git add astro.py

--- astro.py
+++ astro.py
@@ -1,14 +1,17 @@
 import ephem
+from datetime import datetime
 
-def const(planet_name, date_string):            # function name and parameters 
-    planet_class = getattr(ephem, planet_name)  # sets ephem object class 
+def const(planet_name):            # function name and parameters 
+    planet_class = getattr(ephem, planet_name)  # sets ephem object class
+    date_class = datetime.now()
     planet = planet_class()                     # sets planet variable 
     south_bend = ephem.Observer()               # Creates the Observer object 
     south_bend.lat = '41.67'                    # latitude 
     south_bend.lon = '-86.26'                   # west is negative
-    south_bend.date = date_string               # sets date parameter
+    south_bend.date = date_class                # sets date parameter
     planet.compute(south_bend)                  # calculates the location data
-    print ephem.constellation((planet.ra, planet.dec))
-    return planet.alt, planet.az
+    print date_class
+    print planet.alt, planet.az
+    return ephem.constellation((planet.ra, planet.dec))
 
-print const(raw_input('Planet: '), getattr(ephem, now()))
+print const(raw_input('Planet: '))
